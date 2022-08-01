4835a265ebe488ab47cda5c5960dc05488a77cc7
dong <leedong1848@gmail.com>
2022-08-01 20:40:08 +0900

    Change 상수로 변경

--- KrxApi/__init__.py
+++ KrxApi/__init__.py
@@ -1,26 +1,36 @@
 from KrxApi.command import Command
-from KrxApi.payload import PayloadAllStocks, PayloadAllStocksFlRate
+from KrxApi.payload import PayloadStock
 from KrxApi.util.get_time import get_formatted_date_today, get_formatted_date_week_before
+from KrxApi.resources import *
 
 
 class Krx:
     def get_all_stocks_price(self,
-                             mktid="ALL",
-                             trddd=get_formatted_date_today(),
-                             share="1",
-                             money="1"):
-        pl = PayloadAllStocks(mktid, trddd, share, money)
+                             mktId=MARKETS.ALL,
+                             trdDd=get_formatted_date_today(),
+                             share=SHARE.ONE,
+                             money=MONEY.WON):
+        pl = PayloadStock()
+        pl.mktId = mktId
+        pl.trdDd = trdDd
+        pl.share = share
+        pl.money = money
         cmd = Command(pl)
         result = cmd.execute()
         return result
 
     def get_all_stocks_flrate(self,
-                              mktid="ALL",
-                              strtdd=get_formatted_date_week_before(),
-                              enddd=get_formatted_date_today(),
-                              share="1",
-                              money="1"):
-        pl = PayloadAllStocksFlRate(mktid, strtdd, enddd, share, money)
+                              mktId=MARKETS.ALL,
+                              strtDd=get_formatted_date_week_before(),
+                              endDd=get_formatted_date_today(),
+                              share=SHARE.ONE,
+                              money=MONEY.WON):
+        pl = PayloadStock()
+        pl.mktId = mktId
+        pl.share = share
+        pl.money = money
+        pl.endDd = endDd
+        pl.strtDd = strtDd
         cmd = Command(pl)
         result = cmd.execute()
         return result
