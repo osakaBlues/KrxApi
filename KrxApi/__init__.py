f9896ddfcd59c57aa0c6e02a3b69b75af76404e5
Sung-il Yoon <books13456@gmail.com>
2022-08-01 16:10:14 +0900

    ADD 주식-종목시세-전종목 등락률 불러오는 기능 추가 기본 기간은 1주일로 지정

--- KrxApi/__init__.py
+++ KrxApi/__init__.py
@@ -1,16 +1,27 @@
 from .command import Command
-from .payload import PayloadAllStocks
+from .payload import PayloadAllStocks, PayloadAllStocksFlRate
 from .serializer import Deserializer
-from ._get_time import get_formatted_date
+from ._get_time import get_formatted_date_today, get_formatted_date_week_before
 
 
 class Krx:
     def get_all_stocks_price(self,
                              mktid="ALL",
-                             trddd=get_formatted_date(),
+                             trddd=get_formatted_date_today(),
                              share="1",
                              money="1"):
-        pl = PayloadAllStocks(mktid=mktid, trddd=trddd, share=share, money=money)
+        pl = PayloadAllStocks(mktid, trddd, share, money)
+        cmd = Command(pl)
+        result = cmd.execute()
+        return result
+
+    def get_all_stocks_flrate(self,
+                              mktid="ALL",
+                              strtdd=get_formatted_date_week_before(),
+                              enddd=get_formatted_date_today(),
+                              share="1",
+                              money="1"):
+        pl = PayloadAllStocksFlRate(mktid, strtdd, enddd, share, money)
         cmd = Command(pl)
         result = cmd.execute()
         return result
