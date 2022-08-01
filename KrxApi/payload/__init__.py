a01985f3e75f5e323855b33ae4d4731f00eb9d53
dong <leedong1848@gmail.com>
2022-08-01 20:38:48 +0900

    Change payload 기본값 설정, dictionary로 바꾸는 함수 구현

--- KrxApi/payload/__init__.py
+++ KrxApi/payload/__init__.py
@@ -1,58 +1,28 @@
+import inspect
+from KrxApi.resources import *
+
+
 class Payload:
-    _adjStkPrc_check = "Y"
-    _adjStkPrc = "2"
-    _bld = "dbms/MDC/STAT/standard/MDCSTAT01501"
-    _locale = "ko_KR"
-    _mktId = "ALL"
-    _trdDd = "20220728"
-    _strtDd = "20220728"
-    _endDd = "20220728"
-    _share = "1"
-    _money = "1"
-    _csvxls_isNo = "false"
-
     def to_dict(self) -> dict:
-        return {}
+        result = {}
+        for k in dir(self):
+            v = getattr(self, k)
+            if not callable(v) and not k.startswith("_") and v is not None:
+                result[k] = v
+        return result
+    
+
+class PayloadStock(Payload):
+    adjStkPrc_check: str = "Y"
+    adjStkPrc: str = "2"
+    bld: str = None
+    locale: str = LOCALE.KOR
+    mktId: str = MARKETS.ALL
+    trdDd: str = None
+    strtDd: str = None
+    endDd: str = None
+    share: int = SHARE.ONE
+    money: int = MONEY.WON
+    csvxls_isNo: str = "false"
 
 
-class PayloadAllStocks(Payload):
-    def __init__(self, mktid, trddd, share, money):
-        self._mktId = mktid
-        self._trdDd = trddd
-        self._share = share
-        self._money = money
-
-    def to_dict(self):
-        return {
-            "bld": self._bld,
-            "locale": self._locale,
-            "mktId": self._mktId,
-            "trdDd": self._trdDd,
-            "share": self._share,
-            "money": self._money,
-            "csvxls_isNo": self._csvxls_isNo
-        }
-
-
-class PayloadAllStocksFlRate(Payload):
-    def __init__(self, mktid, strtdd, enddd, share, money):
-        self._bld = "dbms/MDC/STAT/standard/MDCSTAT01602"
-        self._mktId = mktid
-        self._strtDd = strtdd
-        self._endDd = enddd
-        self._share = share
-        self._money = money
-
-    def to_dict(self):
-        return {
-            "bld": self._bld,
-            "locale": self._locale,
-            "mktId": self._mktId,
-            "strtDd": self._strtDd,
-            "endDd": self._endDd,
-            "adjStkPrc_check": self._adjStkPrc_check,
-            "adjStkPrc": self._adjStkPrc,
-            "share": self._share,
-            "money": self._money,
-            "csvxls_isNo": self._csvxls_isNo
-        }
