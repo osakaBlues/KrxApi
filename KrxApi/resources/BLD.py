eab0754b5653ab21ced9bda6eef4f90977a2e285
Sung-il Yoon <books13456@gmail.com>
2022-08-02 11:06:52 +0900

    Add : bld 추가

--- KrxApi/resources/BLD.py
+++ KrxApi/resources/BLD.py
@@ -1,1 +1,16 @@
+"""종목 시세"""
 ALL_STOCK = 'dbms/MDC/STAT/standard/MDCSTAT01501'
+ALL_STOCK_FLRATE = "dbms/MDC/STAT/standard/MDCSTAT01602"
+INDV_STOCK_PRICE = "dbms/MDC/STAT/standard/MDCSTAT01701"
+
+"""종목 정보"""
+# 전종목 기본 정보
+ALL_STOCK_INFO = "dbms/MDC/STAT/standard/MDCSTAT01901"
+# 전종목 지정 내역
+ALL_STOCK_DESIG_INFO = "dbms/MDC/STAT/standard/MDCSTAT02001"
+
+""" 개별 종목 종합 정보 """
+# 차트
+INDV_STOCK_INFO_CHART = "dbms/MDC/STAT/standard/MDCSTAT02105"
+# 종목정보
+INDV_STOCK_INFO_GENERAL = "dbms/MDC/STAT/standard/MDCSTAT02101"
