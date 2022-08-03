b7c623d0ebad05d8adc219f5f977c376ba96bec1
Sung-il Yoon <books13456@gmail.com>
2022-08-03 13:05:35 +0900

    Add search_a_stock 구현

--- KrxApi/__init__.py
+++ KrxApi/__init__.py
@@ -11,25 +11,25 @@ class Krx:
     def get_all_stock_prices(mktId=MARKETS.ALL,
                              trdDd=get_formatted_date_today()):
         return Command(
-                Connection(
-                    (PayloadStockBuilder()
-                     .set_bld(BLD.ALL_STOCKS)
-                     .set_mktId(mktId)
-                     .set_trdDd(trdDd)
-                     .build()), URLS.STOCK_INFO_CMD)).execute()
+            Connection(
+                (PayloadStockBuilder()
+                 .set_bld(BLD.ALL_STOCKS)
+                 .set_mktId(mktId)
+                 .set_trdDd(trdDd)
+                 .build()), URLS.STOCK_INFO_CMD)).execute()
 
     @staticmethod
     def get_all_stock_fluctuation_rates(mktId=MARKETS.ALL,
                                         strtDd=get_formatted_date_week_before(),
                                         endDd=get_formatted_date_today()):
         return Command(
-                Connection(
-                    (PayloadStockBuilder()
-                     .set_bld(BLD.ALL_STOCK_FLUCTUATION_RATES)
-                     .set_mktId(mktId)
-                     .set_trdDd(strtDd)
-                     .set_endDd(endDd)
-                     .build()), URLS.STOCK_INFO_CMD)).execute()
+            Connection(
+                (PayloadStockBuilder()
+                 .set_bld(BLD.ALL_STOCK_FLUCTUATION_RATES)
+                 .set_mktId(mktId)
+                 .set_trdDd(strtDd)
+                 .set_endDd(endDd)
+                 .build()), URLS.STOCK_INFO_CMD)).execute()
 
     def get_stock_price(self,
                         ticker,
@@ -49,22 +49,23 @@ class Krx:
     """
     검색에서는 mktId 대신 mktsel
     consonant 1~15 정수 : ㄱ, ㄴ, ㄷ, ... , ㅎ, a~z 까지 15개
+    typeNo는 뭔지 알수없음
     """
 
-    def search_indv_stock(self,
-                          mktsel="ALL",
-                          typeNO="0",
-                          searchText="",
-                          consonant=None):
-        pl = PayloadStock()
-        """
-        todo
-        payload에 데이터 넣기
-        """
-        pl.bld = BLD.A_STOCK_PRICE
-        cmd = Command(pl)
-        result = cmd.execute()
-        return result
+    @staticmethod
+    def search_a_stock(mktsel=MARKETS.ALL,
+                       typeNo="0",
+                       searchText="",
+                       consonant=None):
+        return Command(
+            Connection(
+                (PayloadStockBuilder()
+                 .set_bld(BLD.SEARCH_A_STOCK)
+                 .set_mktsel(mktsel)
+                 .set_typeNo(typeNo)
+                 .set_searchText(searchText)
+                 .set_consonant(consonant)
+                 .build()), URLS.STOCK_INFO_CMD)).execute()
 
 
 __all__ = ["Krx"]
