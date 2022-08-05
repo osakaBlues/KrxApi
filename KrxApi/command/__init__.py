fb41fffe4f576f8bd43718fb61f404ec0cd21fe5
Sung-il Yoon <books13456@gmail.com>
2022-08-05 16:51:11 +0900

    수정중

--- KrxApi/command/__init__.py
+++ KrxApi/command/__init__.py
@@ -4,11 +4,12 @@ from KrxApi.connection import Connection
 
 
 class Command:
-    __slots__ = ['deserializer', 'connection']
+    __slots__ = ['deserializer', 'connection', 'krxResponse']
 
-    def __init__(self, connection: Connection):
+    def __init__(self, connection: Connection, **kwargs):
         self.deserializer: Deserializer = Deserializer()
         self.connection: Connection = connection
+        self.krxResponse: KrxResponse = KrxResponse()
 
     def execute(self) -> KrxResponse:
         if self.connection is None:
@@ -17,4 +18,4 @@ class Command:
             result = self.deserializer.deserialize(self.connection.get_data())
         except DeserializeError:
             raise ConnectionError
-        return KrxResponse(result)
+        return self.krxResponse.get_data(result)
