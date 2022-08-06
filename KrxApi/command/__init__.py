e8b450c061aad6b437c80057255a895482f014aa
Sung-il Yoon <books13456@gmail.com>
2022-08-06 22:54:21 +0900

    Add get_all_stock_price 클래스화

--- KrxApi/command/__init__.py
+++ KrxApi/command/__init__.py
@@ -4,12 +4,11 @@ from KrxApi.connection import Connection
 
 
 class Command:
-    __slots__ = ['deserializer', 'connection', 'krxResponse']
+    __slots__ = ['deserializer', 'connection']
 
-    def __init__(self, connection: Connection, **kwargs):
+    def __init__(self, connection: Connection):
         self.deserializer: Deserializer = Deserializer()
         self.connection: Connection = connection
-        self.krxResponse: KrxResponse = KrxResponse()
 
     def execute(self) -> KrxResponse:
         if self.connection is None:
@@ -18,4 +17,14 @@ class Command:
             result = self.deserializer.deserialize(self.connection.get_data())
         except DeserializeError:
             raise ConnectionError
-        return self.krxResponse.get_data(result)
+        return KrxResponse(result)
+
+    def get_result(self):
+        if self.connection is None:
+            raise NotImplementedError
+        try:
+            result = self.deserializer.deserialize(self.connection.get_data())
+        except DeserializeError:
+            raise ConnectionError
+        return result
+
